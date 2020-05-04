ec0ed533b39bae6b6b6676537240504a1148fc7b
Raphael Gastrock <raphaelgastrock@gmail.com>
2020-05-04 17:25:36 -0400

    order effects analysis and organizing of scripts

---
+++ ana/shared.R
@@ -0,0 +1,158 @@
+library(tidyr)
+library(RColorBrewer)
+library(svglite)
+library(scales)
+library(ez)
+
+#Generic Functions----
+
+getGroupParticipants <- function(group) {
+  
+  #added header=F and the next two lines because this sheet was manually generated
+  all_part <- read.csv(file = "data/pilot/SELECTED/participants_files.csv", header=FALSE)
+  all_part <- all_part[-1,]
+  colnames(all_part) <- c('id','folder')
+  #return all participant ID's for whichever group specified
+  participants_grouped <- as.vector(all_part$id[which(all_part$folder == group)]) 
+  return (participants_grouped)
+  
+}
+
+getParticipantTaskData <- function(group, id, taskno, task) {
+  
+  if (id < 10){
+    filepath <- sprintf('data/pilot/SELECTED/%s/p00%d/p00%d-%d-%s_selected.txt', group, id, id, taskno,task) #creates the file path relative to current directory
+  } else{
+    filepath <- sprintf('data/pilot/SELECTED/%s/p0%d/p0%d-%d-%s_selected.txt', group, id, id, taskno,task) #creates the file path relative to current directory
+  }
+  
+  df <- read.table(file = filepath) #these files need headers to be added
+  # count the number of columns
+  CountCol <- ncol(df)
+  
+  
+  # 19 columns, use these as headers
+  if (CountCol == 19) {
+    colnames(df) <- c("cursorx_cm", "cursory_cm", "homex_cm", "homey_cm", "mousex_cm", "mousey_cm",
+                      "participant", "rotation", "step", "targetangle_deg", "targetx_cm", "targety_cm",
+                      "time_ms", "trial", "trialselected_bool", "sampleselected_bool", "sampleinterpolated_bool",
+                      "maxvelocity_idx", "unsure")
+  }
+  return(df)
+}
+
+rotateTrajectory <- function(X,Y,angle) {
+  
+  # create rotation matrix to rotate the X,Y coordinates
+  th <- (angle/180) * pi
+  R <- t(matrix(data=c(cos(th),sin(th),-sin(th),cos(th)),nrow=2,ncol=2))
+  
+  # put coordinates in a matrix as well
+  coordinates <- matrix(data=c(X,Y),ncol=2)
+  
+  # rotate the coordinates
+  Rcoordinates <- coordinates %*% R
+  
+  # return the rotated reach
+  return(Rcoordinates)
+  
+}
+
+getBSConfidenceInterval <- function(data, resamples) {
+  
+  data <- data[which(is.finite(data))] #need is.finite due to NA values
+  
+  #bootstrap to 95% CI with replacement (done when normal t-distribution is not assumed)
+  #so multiplies data times 1000 and replaces the values
+  samplematrix <- matrix(sample(data, size = resamples*length(data), replace = TRUE), nrow = resamples)
+  #apply mean function to this new matrix
+  BS <- apply(samplematrix, c(1), FUN=mean) #use mean instead of median (we mainly use mean for analyses, even though median is robust to outliers)
+  #95% confidence that data falls within range
+  #2.5% to 97.5%, with 50% being the 'median mean', which is close to actual mean
+  return(quantile(BS, probs = c(0.025, 0.50, 0.975)))
+  
+}
+
+t.interval <- function(data, variance = var(data, na.rm = TRUE), conf.level = 0.95) {
+  #same as getConfidenceInterval, but assumes a normal t-distribution
+  
+  z <- qt((1 - conf.level)/2, df = length(data) - 1, lower.tail = FALSE)
+  
+  xbar <- mean(data, na.rm = TRUE)
+  sdx <- sqrt(variance/length(data))
+  
+  return(c(xbar - z * sdx, xbar, xbar + z * sdx)) 
+  
+}
+
+getColourScheme <- function(groups = c('noninstructed','instructed')){
+  #create a list containing the colourscheme per group
+  for (group in groups){
+    colourscheme <- list()
+    
+    #colourscheme[['WASH0']] <- list('S'='#ff8200ff', # pure orange
+    #                                     'T'='#ff82002f')    #2f gives a lighter shade of the color
+    
+    colourscheme[['noninstructed']] <- list('S'='#e51636ff', #vivid/york red
+                                            'T'='#e516362f')
+    
+    #colourscheme[['WASH1']] <- list('S'='#c400c4ff', #strong magenta
+    #                                     'T'='#c400c42f')
+    
+    colourscheme[['instructed']] <-   list('S'='#005de4ff', #pure blue
+                                           'T'='#005de42f')
+    
+    #colourscheme[['ALIGNED']] <-   list('S'='#A9A9A9ff', #dark grey
+    #                               'T'='#A9A9A92f')
+    
+  }
+  return(colourscheme)
+}
+
+getPtypeColourScheme <- function(perturb = c('ROT','MIR')){
+  #create a list containing the colourscheme per group
+  for (ptype in perturb){
+    colourscheme <- list()
+    
+    #colourscheme[['WASH0']] <- list('S'='#ff8200ff', # pure orange
+    #                                     'T'='#ff82002f')    #2f gives a lighter shade of the color
+    
+    colourscheme[['ROT']] <- list('S'='#e51636ff', #vivid/york red
+                                  'T'='#e516362f')
+    
+    #colourscheme[['WASH1']] <- list('S'='#c400c4ff', #strong magenta
+    #                                     'T'='#c400c42f')
+    
+    colourscheme[['MIR']] <-   list('S'='#005de4ff', #pure blue
+                                    'T'='#005de42f')
+    
+    #colourscheme[['ALIGNED']] <-   list('S'='#A9A9A9ff', #dark grey
+    #                               'T'='#A9A9A92f')
+    
+  }
+  return(colourscheme)
+}
+
+getCtypeColourScheme <- function(conditions = c(1,2)){
+  #create a list containing the colourscheme per group
+  for (condition in conditions){
+    colourscheme <- list()
+    
+    #colourscheme[['WASH0']] <- list('S'='#ff8200ff', # pure orange
+    #                                     'T'='#ff82002f')    #2f gives a lighter shade of the color
+    
+    colourscheme[[1]] <- list('S'='#e51636ff', #vivid/york red
+                              'T'='#e516362f')
+    
+    #colourscheme[['WASH1']] <- list('S'='#c400c4ff', #strong magenta
+    #                                     'T'='#c400c42f')
+    
+    colourscheme[[2]] <-   list('S'='#005de4ff', #pure blue
+                                'T'='#005de42f')
+    
+    #colourscheme[['ALIGNED']] <-   list('S'='#A9A9A9ff', #dark grey
+    #                               'T'='#A9A9A92f')
+    
+  }
+  return(colourscheme)
+}
